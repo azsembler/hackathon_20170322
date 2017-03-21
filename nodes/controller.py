53c75e0cfcd2d10bd4e5667f00a338f3c816225c
ZahidSE <zahid.se@live.com>
2017-03-21 22:50:51 +0100

    Added another topic to start/stop youbot

--- nodes/controller.py
+++ nodes/controller.py
@@ -3,41 +3,65 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
+gazeboOn = False
+
 def move_callback(message):
     command = message.data.lower()
     twist = Twist()
 
-    if  command == "forward":
-        rospy.loginfo("Moving forward ...")
-        twist.linear.x = 0.1
+    rospy.loginfo("Swith is %s" %("ON" if gazeboOn else "OFF"))
 
-    elif command == "backward":
-        rospy.loginfo("Moving backward ...")
-        twist.linear.x = -0.1
+    if gazeboOn:
+        if  command == "forward":
+            rospy.loginfo("Moving forward ...")
+            twist.linear.x = 0.1
 
-    elif command == "left":
-        rospy.loginfo("Moving left ...")
-        twist.linear.x = 0.1
-        twist.angular.z = 0.2
+        elif command == "backward":
+            rospy.loginfo("Moving backward ...")
+            twist.linear.x = -0.1
 
-    elif command == "right":
-        rospy.loginfo("Moving right ...")
-        twist.linear.x = 0.1
-        twist.angular.z = -0.2
+        elif command == "left":
+            rospy.loginfo("Moving left ...")
+            twist.linear.x = 0.1
+            twist.angular.z = 0.2
 
-    elif command == "stop":
-        rospy.loginfo("Stoping ...")
-        twist.linear.x = 0
-        twist.angular.z = 0
+        elif command == "right":
+            rospy.loginfo("Moving right ...")
+            twist.linear.x = 0.1
+            twist.angular.z = -0.2
+
+    else:
+        rospy.loginfo("Gazebo is turned off ...")
 
 
     gazebo_publisher.publish(twist)
+
+def trigger_callback(message):
+    global gazeboOn
+    command = message.data.lower()
+
+    if command == "e_start":
+        rospy.loginfo("Turning on ...")
+        gazeboOn = True
+    elif command == "e_stop":
+        rospy.loginfo("Turning off ...")
+        gazeboOn = False
+
+        twist = Twist()
+
+        twist.linear.x = 0
+        twist.angular.z = 0
+
+        gazebo_publisher.publish(twist)
+
 
 def main():
     global gazebo_publisher
 
     rospy.init_node('controller')
     rospy.Subscriber('/input', String, move_callback)
+
+    rospy.Subscriber('/event_in', String, trigger_callback)
 
     gazebo_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
