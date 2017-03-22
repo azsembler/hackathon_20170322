e2a24db481ebdcff099eccec13c236958fe52c4a
ZahidSE <zahid.se@live.com>
2017-03-22 09:51:00 +0100

    Keep track of last twist value

--- nodes/controller.py
+++ nodes/controller.py
@@ -3,67 +3,76 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-gazeboOn = False
+youBotOn = False
 
 def move_callback(message):
+    global lastTwist
     command = message.data.lower()
     twist = Twist()
 
-    rospy.loginfo("Swith is %s" %("ON" if gazeboOn else "OFF"))
+    rospy.loginfo("Swith is %s" %("ON" if youBotOn else "OFF"))
 
-    if gazeboOn:
-        if  command == "forward":
-            rospy.loginfo("Moving forward ...")
-            twist.linear.x = 0.1
+    if  command == "forward":
+        rospy.loginfo("Moving forward ...")
+        twist.linear.x = 0.1
 
-        elif command == "backward":
-            rospy.loginfo("Moving backward ...")
-            twist.linear.x = -0.1
+    elif command == "backward":
+        rospy.loginfo("Moving backward ...")
+        twist.linear.x = -0.1
 
-        elif command == "left":
-            rospy.loginfo("Moving left ...")
-            twist.linear.x = 0.1
-            twist.angular.z = 0.2
+    elif command == "left":
+        rospy.loginfo("Moving left ...")
+        twist.linear.x = 0.1
+        twist.angular.z = 0.2
 
-        elif command == "right":
-            rospy.loginfo("Moving right ...")
-            twist.linear.x = 0.1
-            twist.angular.z = -0.2
+    elif command == "right":
+        rospy.loginfo("Moving right ...")
+        twist.linear.x = 0.1
+        twist.angular.z = -0.2
 
+    lastTwist = twist
+
+    if youBotOn:
+        youBot_publisher.publish(twist)
     else:
-        rospy.loginfo("Gazebo is turned off ...")
-
-
-    gazebo_publisher.publish(twist)
+        rospy.loginfo("youBot is off")
 
 def trigger_callback(message):
-    global gazeboOn
+    global youBotOn
     command = message.data.lower()
 
     if command == "e_start":
         rospy.loginfo("Turning on ...")
-        gazeboOn = True
+        youBotOn = True
+
+        youBot_publisher.publish(lastTwist)
     elif command == "e_stop":
         rospy.loginfo("Turning off ...")
-        gazeboOn = False
+        youBotOn = False
 
         twist = Twist()
 
         twist.linear.x = 0
         twist.angular.z = 0
 
-        gazebo_publisher.publish(twist)
+        youBot_publisher.publish(twist)
 
 
 def main():
-    global gazebo_publisher
+    global youBot_publisher
+    global lastTwist
+
+    lastTwist = Twist()
+
+    lastTwist.linear.x=0
+    lastTwist.angular.z=0
 
     rospy.init_node('controller')
     rospy.Subscriber('/input', String, move_callback)
 
     rospy.Subscriber('/event_in', String, trigger_callback)
 
-    gazebo_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    youBot_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
